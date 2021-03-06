29002cfde7d55c67d1ebb07fcbd162cb85d3678d
Selim Agovic <selimagovic@gmail.com>
2021-03-06 22:04:52 +0100

    Fixed Minor Errors

--- Assets/3RD_Person_Controller/Scripts/PlayerController.cs
+++ Assets/3RD_Person_Controller/Scripts/PlayerController.cs
@@ -5,128 +5,140 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-  #region Variables
-  //[SerializeField]
-  //private float _speed = 2f;
-  private float _jumpDirection;
-  [SerializeField]
+    #region Variables
+    //[SerializeField]
+    //private float _speed = 2f;
+    private float _jumpDirection;
+    [SerializeField]
+    private float _maxFwdSpeed = 8f;
+    [SerializeField]
+    private float _turnSpeed = 100f;
+    float _desiredSpeed;
+    float _forwardSpeed;
+    float _groundRayDist = 0.5f;
+    const float _groundAccel = 5f;
+    const float _groundDecel = 25f;
 
-  private float _maxFwdSpeed = 8f;
-  [SerializeField]
-  private float _turnSpeed = 100f;
-  float _desiredSpeed;
-  float _forwardSpeed;
-  float _groundRayDist = 2f;
-  const float _groundAccel = 5f;
-  const float _groundDecel = 25f;
+    float _jumpEffort=0;
 
-  [SerializeField]
-  float _jumpSpeed = 30000f;
-  bool readyJump = false;
-  bool _onGround = true;
+    [SerializeField]
+    float _jumpSpeed = 30000f;
+    [SerializeField]
+    [Range(1, 4)]
+    float _maxJumpEffort = 1;
+    bool readyJump = false;
+    bool _onGround = true;
 
-  Animator _animator;
-  Rigidbody _rb;
+    Animator _animator;
+    Rigidbody _rb;
 
-  bool isMoveInput
-  {
-    get { return !Mathf.Approximately(moveDirection.sqrMagnitude, 0f); }
-  }
-  Vector2 moveDirection;
-  #endregion
-  // Start is called before the first frame update
-  void Start()
-  {
-    _animator = GetComponent<Animator>();
-    if (_animator == null)
+    bool isMoveInput
     {
-      Debug.LogError("No Animator Component attached to Parent Game Object!!!");
+        get { return !Mathf.Approximately(moveDirection.sqrMagnitude, 0f); }
+    }
+    Vector2 moveDirection;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        if (_animator == null)
+        {
+            Debug.LogError("No Animator Component attached to Parent Game Object!!!");
 
+        }
+
+        _rb = GetComponent<Rigidbody>();
+        if (_rb == null)
+            Debug.LogError("No Rigidbody attached to Parent Game Object!!!");
     }
 
-    _rb = GetComponent<Rigidbody>();
-    if (_rb == null)
-      Debug.LogError("No Rigidbody attached to Parent Game Object!!!");
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    CalculateMovement(moveDirection);
-    Jump(_jumpDirection);
-    RaycastHit hit;
-    Ray ray = new Ray(transform.position + Vector3.up * _groundRayDist * 0.5f, -Vector3.up);
-    if (Physics.Raycast(ray, out hit, _groundRayDist))
+    // Update is called once per frame
+    void Update()
     {
-      if (!_onGround)
-      {
-        _onGround = true;
-        _animator.SetBool("Land", true);
-      }
+        CalculateMovement(moveDirection);
+        Jump(_jumpDirection);
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position + Vector3.up * _groundRayDist*0.5f, -Vector3.up);
+        if (Physics.Raycast(ray, out hit, _groundRayDist))
+        {
+            if (!_onGround)
+            {
+                _onGround = true;
+                _animator.SetFloat("LandingVelocity", _rb.velocity.magnitude);
+                _animator.SetBool("Land", true);
+                _animator.SetBool("Falling", false);
+            }
+        }
+        else
+        {
+            _onGround = false;
+            _animator.SetBool("Falling", true);
+            _animator.applyRootMotion = false;
+        }
+        Debug.DrawRay(transform.position + Vector3.up * _groundRayDist * 0.5f, -Vector3.up * _groundRayDist, Color.red);
     }
-    else
+    #region Custom Methods
+    public void OnMove(InputAction.CallbackContext context)
     {
-      _onGround = false;
-      _animator.SetBool("Land", false);
+        moveDirection = context.ReadValue<Vector2>();
     }
-    Debug.DrawRay(transform.position + Vector3.up * _groundRayDist * 0.5f, -Vector3.up * _groundRayDist, Color.red);
-  }
-  #region Custom Methods
-  public void OnMove(InputAction.CallbackContext context)
-  {
-    moveDirection = context.ReadValue<Vector2>();
-  }
-  public void OnJump(InputAction.CallbackContext context)
-  {
-    _jumpDirection = context.ReadValue<float>();
-  }
-
-  void CalculateMovement(Vector2 direction)
-  {
-    float turnAmount = direction.x;
-    float fDirection = direction.y;
-
-    if (direction.sqrMagnitude > 1f)
+    public void OnJump(InputAction.CallbackContext context)
     {
-      direction.Normalize();
+        _jumpDirection = context.ReadValue<float>();
     }
-    _desiredSpeed = direction.magnitude * _maxFwdSpeed * Mathf.Sin(fDirection);
-    float accel = isMoveInput ? _groundAccel : _groundDecel;
 
-    _forwardSpeed = Mathf.MoveTowards(_forwardSpeed, _desiredSpeed, accel *
-    Time.deltaTime);
-
-    _animator.SetFloat("ForwardSpeed", _forwardSpeed);
-
-    transform.Rotate(0, turnAmount * _turnSpeed * Time.deltaTime, 0);
-    //transform.Translate(direction.x * _speed * Time.deltaTime, 0, direction.y * _speed * Time.deltaTime);
-  }
-
-
-  void Jump(float direction)
-  {
-    if (direction > 0)
+    void CalculateMovement(Vector2 direction)
     {
-      _animator.SetBool("ReadyJump", true);
-      readyJump = true;
+        float turnAmount = direction.x;
+        float fDirection = direction.y;
+
+        if (direction.sqrMagnitude > 1f)
+        {
+            direction.Normalize();
+        }
+        _desiredSpeed = direction.magnitude * _maxFwdSpeed * Mathf.Sin(fDirection);
+        float accel = isMoveInput ? _groundAccel : _groundDecel;
+
+        _forwardSpeed = Mathf.MoveTowards(_forwardSpeed, _desiredSpeed, accel *
+        Time.deltaTime);
+
+        _animator.SetFloat("ForwardSpeed", _forwardSpeed);
+
+        transform.Rotate(0, turnAmount * _turnSpeed * Time.deltaTime, 0);
+        //transform.Translate(direction.x * _speed * Time.deltaTime, 0, direction.y * _speed * Time.deltaTime);
     }
-    else if (readyJump)
+
+
+    void Jump(float direction)
     {
-      _animator.SetBool("Launch", true);
-      readyJump = false;
+        if (direction > 0)
+        {
+            _animator.SetBool("ReadyJump", true);
+            readyJump = true;
+            _jumpEffort += Time.deltaTime;
+            Debug.Log(_jumpEffort);
+        }
+        else if (readyJump)
+        {
+            _animator.SetBool("Launch", true);
+            readyJump = false;
+            _animator.SetBool("ReadyJump", false);
+        }
+        
     }
-  }
-  public void Launch()
-  {
-    _rb.AddForce(0, _jumpSpeed, 0);
-    _animator.SetBool("Launch", false);
-    _animator.applyRootMotion = false;
-  }
-  public void Land()
-  {
-    _animator.SetBool("ReadyJump", false);
-    _animator.SetBool("Land", false);
-    _animator.applyRootMotion = true;
-  }
-  #endregion
+    public void Launch()
+    {
+        _rb.AddForce(0, _jumpSpeed * Mathf.Clamp(_jumpEffort,1,_maxJumpEffort), 0);
+        _animator.SetBool("Launch", false);
+        _animator.applyRootMotion = false;
+    }
+    public void Land()
+    {
+        _animator.SetBool("Land", false);
+        _animator.applyRootMotion = true;
+        _animator.SetBool("Launch", false);
+        _jumpEffort = 0;
+    }
+    #endregion
 }
